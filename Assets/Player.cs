9d98ddccb246818098499b29e20d51a07635dd5e
Hưng <hunghdqpk043359@gmail.com>
2026-01-14 19:03:02 +0700

    HealthBar

---
+++ Assets/Player.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    private void Update()
+    {
+        // Example damage input for testing
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
