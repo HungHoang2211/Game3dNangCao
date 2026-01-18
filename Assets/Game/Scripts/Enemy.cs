81d10cb357362390033c7333134934e3d33912d9
Hoang <hoanggtdpk04069@gmail.com>
2026-01-18 10:36:06 +0700

    Map + AI

---
+++ Assets/Game/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public EnemyStateMachine stateMachine { get; private set; }
+
+    public EnemyState idleState { get; private set; }
+    public EnemyState moveState { get; private set; }
+    void Start()
+    {
+        stateMachine = new EnemyStateMachine();
+
+        idleState = new EnemyState(this, stateMachine, "Idle");
+        moveState = new EnemyState(this, stateMachine, "Move");
+
+        stateMachine.Initialize(idleState);
+    }
+
+
+    void Update()
+    {
+        stateMachine.currentState.Update();
+
+        if (Input.GetKeyDown(KeyCode.V))
+            stateMachine.ChangeState(idleState);
+
+        if (Input.GetKeyDown(KeyCode.C))
+            stateMachine.ChangeState(moveState);
+    }
+}
