ea92e7561535598cc19eed2fbf4dc06f8a00f628
Vuddpk04259 <vuddpk04259@gmail.com>
2026-02-01 03:39:18 +0700

    EnemyVuControlller + camera

--- Assets/Game/Scripts/Vu/CharacterControllerVu.cs
+++ Assets/Game/Scripts/Vu/CharacterControllerVu.cs
@@ -1,16 +1,63 @@
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CheracterVu : MonoBehaviour
+public class CharacterControllerVu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody rb;
+    private bool isGrounded = false;
+
+    public Animator animator;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+ 
     void Update()
     {
-        
+        HandleMovement();
+    }
+    public void HandleMovement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        if (movement.magnitude > 1)
+        {
+            movement.Normalize();
+             
+            
+        }
+        if (movement == Vector3.zero)
+        {
+            animator.SetBool("isWalkingVu", false);
+        }
+        else 
+        {
+            animator.SetBool("isWalkingVu", true); 
+        }
+
+        rb.MovePosition(transform.position + movement * 5 * Time.fixedDeltaTime);
+
+        HandleRotation(movement);
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    public void HandleRotation(Vector3 enemyVuMovementInput)
+    {
+        Vector3 lookDirection = enemyVuMovementInput;
+        lookDirection.y = 0; 
+
+        if(lookDirection != Vector3.zero)
+        {
+            Quaternion rotation = Quaternion.LookRotation(lookDirection);
+            transform.rotation = rotation;
+        }
     }
 }
