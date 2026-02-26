7a552f6ed579f136dfef8cde8dbcb3bb20ab8260
Hưng <hunghdqpk043359@gmail.com>
2026-02-26 14:53:03 +0700

    tiền

---
+++ Assets/CurrencyData.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCurrency", menuName = "Inventory/Currency")]
+public class CurrencyData : ScriptableObject
+{
+    public string currencyName;
+    public Sprite currencyIcon;
+    public int amount; // Số lượng tiền hiện có
+
+    public void Add(int value)
+    {
+        amount += value;
+    }
+
+    public bool TrySpend(int value)
+    {
+        if (amount >= value)
+        {
+            amount -= value;
+            return true;
+        }
+        return false;
+    }
+}
